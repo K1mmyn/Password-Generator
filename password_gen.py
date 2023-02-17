892cefe12510734d1e566675815b2018327d9e55
K1mmyn <125098192+K1mmyn@users.noreply.github.com>
2023-02-17 21:02:39 +1300

    now you cant have more min number then password length

--- password_gen.py
+++ password_gen.py
@@ -6,27 +6,26 @@ letters = list(string.ascii_letters)
 special_letters = ["!", '@', "#", "$", "%", "^", "&", '*', "(", ")", "-", "_", "+", "=", '{', "}", "[", "]", ":", ";", "'", '"', '<', ">", "?", "/", "|", "\\", ",", "."]
 passwordlist = []
 H = 0
-ask = int(input("\nMininum Numbers: "))
-ask2 = int(input("\nMininum Speical Letters: "))
-how_many = int(input("\nPassword Length: "))
+while True:
+    ask = int(input("\nMininum Numbers: "))
+    ask2 = int(input("Mininum Speical Letters: "))
+    how_many = int(input("Password Length: "))
+    while True:
+        if ask + ask2 >= how_many:
+            print(red("\nPlease enter a lower amount of number or speical letters ", 'bold'))
+            break
 
-x = ask + 2
-y = ask2 + 2
+        for number in range(random.randint(ask, (ask + 2))):
+            passwordlist += str(random.randint(0, 10)); H += 1
 
-for number in range(0, x):
-    passwordlist += str(random.randint(0, 10))
-    H += 1
+        for number in range(random.randint(ask, (ask + 2))):
+            passwordlist += random.choice(special_letters); H += 1
 
-for number in range(0, y):
-    passwordlist += random.choice(special_letters)
-    H += 1
+        ask3 = how_many - H; z = ask3 + 1
 
-ask3 = how_many - H
-z = ask3 + 1
+        for number in range(z):
+            passwordlist += random.choice(letters)
 
-for number in range(z):
-    passwordlist += random.choice(letters)
-
-random.shuffle(passwordlist)
-password = ("".join(passwordlist))
-print(f"\nYour Randomly generated password is: {password} \n")
+        random.shuffle(passwordlist)
+        password = ("".join(passwordlist))
+        print(f"\nYour Randomly generated password is: {password} \n"); quit()
